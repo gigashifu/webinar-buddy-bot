b5c2d75b35e20510990b545cf78accb0c84a1677
Neel Santhumayor <neel40133@gmail.com>
2025-10-04 16:43:54 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,8 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "0.0.0.0",
-    port: 5000,
-    hmr: {
-      clientPort: 443,
-    }
+    host: "::",
+    port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
